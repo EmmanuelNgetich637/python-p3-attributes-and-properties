d6bb08eaee58c6da7f584e817955716fedfdecec
Emmanuel Ngetich <engetich333@gmail.com>
2025-05-10 02:42:09 -0700

    Fix and Update Person and Dog Classes: Add Validation for Name and Job

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,29 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed="Pug"):
+        self._name = None
+        self._breed = None
+
+        # Validate and set name
+        if name is not None:
+            if isinstance(name, str) and 1 <= len(name) <= 25:
+                self._name = name
+            else:
+                print("Name must be string between 1 and 25 characters.")
+                self._name = "None"
+        
+        # Validate and set breed
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = "None"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def breed(self):
+        return self._breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,22 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unknown"):
+        self.name = self.validate_name(name)
+        self.job = self.validate_job(job)
+    
+    def validate_name(self, name):
+        if not isinstance(name,str) or len(name) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return None 
+        else:
+            return name.title()
+        
+    def validate_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            return None
+        else:
+            return job
+        
+        
